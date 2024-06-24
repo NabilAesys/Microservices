eb3f6848f4d263a6ec481d972b87ee8f3c255809
nabilAesys <nabil.ezzaki@aesys.tech>
2024-06-24 17:48:20 +0200

    added security dependency

---
+++ api-gateway/src/main/java/com/nbl/apigataway/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.nbl.apigataway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+}

03d371dc08f3b8cb9cec72124fbaea9aa9f83937
nabilAesys <nabil.ezzaki@aesys.tech>
2024-06-26 15:28:15 +0200

    added security config and kayloack

--- api-gateway/src/main/java/com/nbl/apigataway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/nbl/apigataway/config/SecurityConfig.java
@@ -1,10 +1,19 @@
 package com.nbl.apigataway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
-@EnableWebFluxSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
 }
